fe61f28094935b3b95980d2af08d64b0ffe530eb
Samee2026 <78082351+Samee2026@users.noreply.github.com>
2023-01-04 21:30:38 +0530

    Add files via upload

---
+++ Automate File Segregation/Move_File.py
@@ -0,0 +1,37 @@
+import os 
+import shutil
+
+from_dir = "C:/Users/Mohd Samee Alekar/Downloads"
+to_dir = "C:/Users/Mohd Samee Alekar/OneDrive/Desktop/PythonPrograms/Document_Files"
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    print(name)
+    print(extension)
+
+    path1 = from_dir
+    path2 = to_dir
+    path3 = to_dir
+
+    if extension == '':
+        continue
+
+    if extension in ['.gif','.png','.jpg','.jpeg','.jfif']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Document_Files"
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name
+        print("path1", path1)
+        print("path3", path3)
+
+    if os.path.exists(path2):
+        print("Moving" + file_name + ".....")
+        shutil.move(path1, path3)
+    else:
+        os.makedirs(path2)
+        print("Moving" + file_name + ".....")
+        shutil.move(path1, path3)
+
